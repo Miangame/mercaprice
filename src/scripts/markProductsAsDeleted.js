3c9c9fb3e05dbefe5fad83a2ba61538a2d4a545c
Miguel Gavilán <miangame1@gmail.com>
2025-02-09 18:14:37 +0100

    feat: add script for mark products as deleted

---
+++ src/scripts/markProductsAsDeleted.js
@@ -0,0 +1,118 @@
+const fs = require('fs')
+
+const axios = require('axios')
+const xml2js = require('xml2js')
+const { PrismaClient } = require('@prisma/client')
+
+const prisma = new PrismaClient()
+
+const saveToErrorLog = async (message) => {
+  const date = new Date().toISOString()
+  try {
+    await fs.appendFileSync('error.log', `${date}: ${message}\n`, 'utf8')
+  } catch (error) {
+    console.error('Error al guardar en el log de errores:', error.message)
+  }
+}
+
+const sendTelegramMessage = async (message) => {
+  const url = `https://api.telegram.org/bot${process.env.TELEGRAM_BOT_TOKEN}/sendMessage`
+
+  try {
+    await axios.post(url, {
+      chat_id: process.env.TELEGRAM_CHAT_ID,
+      text: message
+    })
+  } catch (error) {
+    await saveToErrorLog(
+      `Error al enviar mensaje de Telegram: ${error.message}`
+    )
+  }
+}
+
+const markProductsAsDeleted = async (urlOrPath) => {
+  const startTime = Date.now()
+  const invalidUrlErrors = []
+  const updateErrors = []
+  let deletedProductsCount = 0
+
+  try {
+    let xmlData
+
+    if (urlOrPath.startsWith('http://') || urlOrPath.startsWith('https://')) {
+      const response = await axios.get(urlOrPath)
+      xmlData = response.data
+    } else {
+      xmlData = fs.readFileSync(urlOrPath, 'utf8')
+    }
+
+    const parser = new xml2js.Parser()
+    const result = await parser.parseStringPromise(xmlData)
+
+    const sitemapProductIds = result.urlset.url
+      .map((entry) => entry.loc[0])
+      .filter((url) =>
+        /^https:\/\/tienda.mercadona.es\/product\/[0-9]+/.test(url)
+      )
+      .map((url) => url.match(/[0-9]+(?:\.[0-9]+)?/)[0])
+
+    console.log('Número de productos en el sitemap:', sitemapProductIds.length)
+
+    const storedProducts = await prisma.product.findMany({
+      where: {
+        deletedAt: null
+      },
+      select: { id: true, externalId: true }
+    })
+
+    const productsToDelete = storedProducts.filter(
+      (p) => !sitemapProductIds.includes(p.externalId)
+    )
+
+    console.log(
+      'Número de productos a marcar como eliminados:',
+      productsToDelete.length
+    )
+
+    for (const product of productsToDelete) {
+      try {
+        await prisma.product.update({
+          where: { id: product.id },
+          data: { deleted: true }
+        })
+        deletedProductsCount++
+      } catch (error) {
+        await saveToErrorLog(
+          `Error al marcar como eliminado el producto ID ${product.externalId}: ${error.message}`
+        )
+        updateErrors.push(`Producto ${product.externalId}: ${error.message}`)
+      }
+    }
+
+    const endTime = Date.now()
+    const executionTimeMs = endTime - startTime
+    const executionTimeSec = (executionTimeMs / 1000).toFixed(2)
+    const executionTimeMin = (executionTimeMs / 60000).toFixed(2)
+
+    const telegramMessage = `
+      🚀 *Actualización de Productos Finalizada* 🚀
+
+      📊 *Resultados*:
+      🔹 Productos en el sitemap: *${sitemapProductIds.length}*
+      ❌ Productos eliminados: *${deletedProductsCount}*
+      ⚠️ Errores al eliminar: *${updateErrors.length}*
+
+      ${updateErrors.length > 0 ? `🛑 *Errores al eliminar productos*: \n${updateErrors.slice(0, 5).join('\n')}\n\n` : ''}
+
+      ⏳ *Tiempo de ejecución:* ${executionTimeMin} min (${executionTimeSec} seg)
+      📅 *Fecha de ejecución:* ${new Date().toLocaleString()}
+    `
+
+    await sendTelegramMessage(telegramMessage)
+  } catch (error) {
+    await saveToErrorLog(`Error general en el script: ${error.message}`)
+    await sendTelegramMessage(`❌ Error en el script: ${error.message}`)
+  }
+}
+
+markProductsAsDeleted('https://tienda.mercadona.es/sitemap.xml')
