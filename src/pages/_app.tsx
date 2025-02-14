2ac645d675b36faccedeb393d4630ec8f7c0b6cf
Miguel Gavilán <miangame1@gmail.com>
2025-02-14 12:52:37 +0100

    fix: component location

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,12 @@ import type { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { Analytics } from '@vercel/analytics/react'
 
-import { Head } from './components/Head/Head'
-import { ResetCSS } from './components/ResetCSS/ResetCSS.styles'
-import { GlobalStyles } from './components/GlobalStyles/GlobalStyles.styled'
-import { Header } from './components/Header/Header'
-import { Footer } from './components/Footer/Footer'
+import { Head } from '../components/Head/Head'
+import { ResetCSS } from '../components/ResetCSS/ResetCSS.styles'
+import { GlobalStyles } from '../components/GlobalStyles/GlobalStyles.styled'
+import { Header } from '../components/Header/Header'
+import { Footer } from '../components/Footer/Footer'
+
 import { theme } from '@/theme/theme'
 
 export default function App({ Component, pageProps }: AppProps) {
